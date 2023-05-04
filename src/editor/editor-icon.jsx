c5b34676bdeeb4c7b92490c7857ec39b0e52c9f0
Vijeth (GitHub Desktop) <38877972+vijethx@users.noreply.github.com>
2023-05-04 19:33:36 +0530

    ui rewamp 1

---
+++ src/editor/editor-icon.jsx
@@ -0,0 +1,72 @@
+export const TemplatesIcon = () => {
+	return (
+		<img
+			src="/sidebar-icons/templates.svg"
+			alt="templates"
+		/>
+	);
+};
+export const NFTIcon = () => {
+	return (
+		<img
+			src="/sidebar-icons/nft.svg"
+			alt="nft"
+		/>
+	);
+};
+export const TextIcon = () => {
+	return (
+		<img
+			src="/sidebar-icons/text.svg"
+			alt="text"
+		/>
+	);
+};
+export const ElementsIcon = () => {
+	return (
+		<img
+			src="/sidebar-icons/elements.svg"
+			alt="elements"
+		/>
+	);
+};
+export const BackgroundIcon = () => {
+	return (
+		<img
+			src="/sidebar-icons/background.svg"
+			alt="background"
+		/>
+	);
+};
+export const UploadIcon = () => {
+	return (
+		<img
+			src="/sidebar-icons/upload.svg"
+			alt="upload"
+		/>
+	);
+};
+export const LayersIcon = () => {
+	return (
+		<img
+			src="/sidebar-icons/layers.svg"
+			alt="layers"
+		/>
+	);
+};
+export const ResizeIcon = () => {
+	return (
+		<img
+			src="/sidebar-icons/resize.svg"
+			alt="resize"
+		/>
+	);
+};
+export const AIIcon = () => {
+	return (
+		<img
+			src="/sidebar-icons/ai.svg"
+			alt="ai"
+		/>
+	);
+};
