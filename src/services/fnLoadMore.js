6c5f2acdebb6f8b59a5ecd5154e180f15ce37e79
Pankaj Kumar <pankajkumardas727@gmail.com>
2023-07-17 19:54:53 +0530

    add: token gating

--- src/services/fnLoadMore.js
+++ src/services/fnLoadMore.js
@@ -1,9 +1,11 @@
 // run this function only in useEffect if you want to load more data by scrolling to the bottom of the page
 export const fnLoadMore = (hasNextPage, fetchNextPage) => {
+  console.log({hasNextPage});
   if (hasNextPage) {
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting) {
+          console.log("visible");
           fetchNextPage();
         }
       },
