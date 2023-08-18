1c8e50298148a08e85a434e9b8fb038edb69a311
Pankaj Kumar <pankajkumardas727@gmail.com>
2023-08-18 22:31:07 +0530

    fix: topbar import error 3

--- src/editor/topbar/topbar.jsx
+++ src/editor/topbar/topbar.jsx
@@ -4,7 +4,7 @@ import { useAccount } from "wagmi";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import RightDrawer from "./rightdrawer";
 
-const Topbar = observer(({ store }) => {
+const Topbar = ({ store }) => {
   const { isConnected } = useAccount();
 
   return (
@@ -56,6 +56,6 @@ const Topbar = observer(({ store }) => {
       </div>
     </div>
   );
-});
+};
 
 export default Topbar;
