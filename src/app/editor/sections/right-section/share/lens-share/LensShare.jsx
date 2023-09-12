0bbc9e7ed172f58764b2dfdeb07750deb0c0778e
Pankaj Kumar <pankajkumardas727@gmail.com>
2023-09-12 23:11:40 +0530

    fix: lens hanlde fixed

--- src/app/editor/sections/right-section/share/lens-share/LensShare.jsx
+++ src/app/editor/sections/right-section/share/lens-share/LensShare.jsx
@@ -681,7 +681,7 @@ const LensShare = () => {
         ...prevEnabled,
         splitRevenueRecipients: [
           {
-            recipient: "@lenspostxyz.test",
+            recipient: "@lenspostxyz.lens",
             split: enabled.splitRevenueRecipients[0]?.split || 10.0,
           },
           ...updatedRecipients,
