02f961210d51a4fee136263dd8c6bd81d4585101
Pankaj Kumar <pankajkumardas727@gmail.com>
2023-11-08 19:10:53 +0530

    fix: dispatcher

--- src/app/editor/sections/right-section/share/lens-share/LensShare.jsx
+++ src/app/editor/sections/right-section/share/lens-share/LensShare.jsx
@@ -79,6 +79,8 @@ const LensShare = () => {
     referralError,
     setReferralError,
     parentRecipientListRef,
+
+    lensAuthState,
   } = useContext(Context);
 
   const [sharing, setSharing] = useState(false);
