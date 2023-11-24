12de38858f36b6c18c16e45e43b3ddbfae632562
Pankaj Kumar <pankajkumardas727@gmail.com>
2023-11-25 03:06:15 +0530

    fix: user address attribution

--- src/hooks/app/useLocalStorage.js
+++ src/hooks/app/useLocalStorage.js
@@ -1,6 +1,45 @@
-// hook for local storage to store the data and retrieve the data
-const useLocalStorage = (key, initialValue) => {
-    // get the data from local storage
+import { LOCAL_STORAGE } from "../../data";
+
+// hook for local storage to retrieve the data
+const useLocalStorage = () => {
+  const getFromLocalStorage = (key) => {
+    try {
+      const serializedValue = localStorage.getItem(key);
+      if (serializedValue === null) return undefined;
+      return JSON.parse(serializedValue);
+    } catch (e) {
+      console.log(e);
+      return undefined;
+    }
+  };
+
+  const authToken = getFromLocalStorage(LOCAL_STORAGE.authToken);
+  const evmAuth = getFromLocalStorage(LOCAL_STORAGE.evmAuth);
+  const solanaAuth = getFromLocalStorage(LOCAL_STORAGE.solanaAuth);
+  const loggedInUserAddress = getFromLocalStorage(LOCAL_STORAGE.userAddress);
+  const userAuthTime = getFromLocalStorage(LOCAL_STORAGE.userAuthTime);
+  const lensAuth = getFromLocalStorage(LOCAL_STORAGE.lensAuth);
+  const ifUserEligible = getFromLocalStorage(LOCAL_STORAGE.ifUserEligible);
+  const hasUserSeenTheApp = getFromLocalStorage(
+    LOCAL_STORAGE.hasUserSeenTheApp
+  );
+  const dispatcher = getFromLocalStorage(LOCAL_STORAGE.dispatcher);
+  const userGuideTour = getFromLocalStorage(LOCAL_STORAGE.userGuideTour);
+  const braveShieldWarn = getFromLocalStorage(LOCAL_STORAGE.braveShieldWarn);
+
+  return {
+    authToken,
+    evmAuth,
+    solanaAuth,
+    loggedInUserAddress,
+    userAuthTime,
+    lensAuth,
+    ifUserEligible,
+    hasUserSeenTheApp,
+    dispatcher,
+    userGuideTour,
+    braveShieldWarn,
+  };
 };
 
 export default useLocalStorage;
