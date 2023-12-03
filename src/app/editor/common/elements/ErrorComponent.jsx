d33a0424ce3ae1e9c9362b1f4242b184c32bc734
Pankaj Kumar <pankajkumardas727@gmail.com>
2023-12-04 01:11:25 +0530

    🤓: removed kachra box

--- src/app/editor/common/elements/ErrorComponent.jsx
+++ src/app/editor/common/elements/ErrorComponent.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { errorMessage } from "../../../../utils/errorMessage";
 import Lottie from "lottie-react";
-import animationData from "../../../../assets/lottie/emptystates/emptyState1.json";
+import animationData from "../../../../assets/lottie/emptystates/emptyState2.json";
 
 const ErrorComponent = ({ error }) => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <Lottie animationData={animationData}/>
+      {/* <Lottie animationData={animationData}/> */}
       <div className="text-center">
         <p className="text-gray-500 text-sm mt-4">{errorMessage(error)}</p>
       </div>
--- src/app/editor/common/elements/MessageComponent.jsx
+++ src/app/editor/common/elements/MessageComponent.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 // const randomNumber = Math.floor(Math.random() * 3) + 1;
 // const animationData = import(`../../../../assets/lottie/emptystates/EmptyState${randomNumber}.json`);
 
-import animationData from '../../../../assets/lottie/emptystates/EmptyState2.json';
+import animationData from '../../../../assets/lottie/emptystates/emptyState3.json';
 
 const MessageComponent = ({message}) => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <Lottie className="h-16 pb-2 sm:h-40" animationData={animationData}/>
+      {/* <Lottie className="h-16 pb-2 sm:h-40" animationData={animationData}/> */}
       <div className="text-center"> 
         <p className="text-gray-500 text-sm mt-4">{message}</p>
       </div>
