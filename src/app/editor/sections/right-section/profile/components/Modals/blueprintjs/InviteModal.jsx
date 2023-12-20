ece12feeeb2f43284c6239e1a25acdbdd8619357
Pankaj Kumar <pankajkumardas727@gmail.com>
2023-12-20 22:05:34 +0530

    working on rewards

--- src/app/editor/sections/right-section/profile/components/Modals/blueprintjs/InviteModal.jsx
+++ src/app/editor/sections/right-section/profile/components/Modals/blueprintjs/InviteModal.jsx
@@ -2,8 +2,6 @@ import React, { useContext, useEffect, useState } from "react";
 import { Dialog, DialogBody, DialogFooter } from "@blueprintjs/core";
 import { Input, Button, Typography } from "@material-tailwind/react";
 import BsX from "@meronex/icons/bs/BsX";
-import { useMutation } from "@tanstack/react-query";
-import { updateUserProfile } from "../../../../../../../../services/apis/BE-apis/backendApi";
 import { Context } from "../../../../../../../../providers/context";
 import {
   getProfileImage,
@@ -14,7 +12,6 @@ import { Top5ProfileCard } from "../../Cards";
 
 // XMTP
 import { Client } from "@xmtp/xmtp-js";
-import "../../../../../../../../../polyfills";
 
 const InviteModal = () => {
   const { address } = useAccount();
@@ -22,91 +19,30 @@ const InviteModal = () => {
   const [formData, setFormData] = useState({});
   const [top5Users, setTop5Users] = useState([]);
   const [loading, setLoading] = useState(false);
-  const { data: walletClient, isError, isLoading } = useWalletClient();
+  const { data: walletClient } = useWalletClient();
   const [xmtpSendAddr, setXmtpSendAddr] = useState("");
 
-  // const { data: signer, isError, isLoading } = useSigner()
+  const XMTPfn = async () => {
+    const xmtp = await Client.create(walletClient, { env: "dev" });
+
+    // Start a conversation with XMTP
+    const conversation = await xmtp.conversations.newConversation(
+      "0x983d6466FAC5B2aFe57e070283a4932A1bd0508F"
+    );
+
+    // Send a message
+    await conversation.send("gm");
+  };
 
   const userAddresses = [];
   const userProfileNames = [];
   const userProfileImages = [];
 
-  // XMPT - START
-  // Create a client
-  let xmtp = null;
-  //Fabri wallet
-  let WALLET_TO = "0x3F11b27F323b62B159D2642964fa27C46C841897";
-  let conversation = null;
-
-  // 0xE3811DeFd98AF92712e54b0b3E1735c1051C86D6
-
-  async function create_a_client() {
-    if (!walletClient) {
-      console.log("Wallet is not initialized");
-      return;
-    }
-
-    xmtp = await Client.create(walletClient, { env: "dev" });
-    console.log("Client created", xmtp.address);
-  }
-
-  //Check if an address is on the network
-  async function check_if_an_address_is_on_the_network() {
-    //Message this XMTP message bot to get an immediate automated reply:
-    // gm.xmtp.eth (0x937C0d4a6294cdfa575de17382c7076b579DC176) 
-    //
-
-    if (xmtp) {
-      const isOnDevNetwork = await xmtp.canMessage(WALLET_TO);
-      console.log(`Can message: ${isOnDevNetwork}`);
-      return isOnDevNetwork;
-    }
-    return false;
-  }
-
-  //Start a new conversation
-  async function start_a_new_conversation() {
-    const canMessage = await check_if_an_address_is_on_the_network();
-    if (!canMessage) {
-      console.log("Address is not on the network");
-      return;
-    }
-
-    if (xmtp) {
-      conversation = await xmtp.conversations.newConversation(WALLET_TO);
-      console.log(conversation)
-      console.log(`Conversation created with ${conversation.peerAddress}`);
-    }
-  }
-
-  //Send a message
-  async function send_a_message() {
-    if (conversation) {
-      const message = await conversation.send("GM Builders");
-      console.log(`Message sent: "${message.content}"`);
-      return message;
-    }
-  }
-
-  // Send a message to XMTP
-  const fnSendXmtpMessage = async () => {
-    await start_a_new_conversation()
-    const res = await send_a_message();
-    console.log(res);
-  };
-
-  // XMPT - END
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    console.log(name, value);
-
-    if (name === "xmtpSendAddr") setXmtpSendAddr(value);
-  };
-
   const fnGetTop5Interactions = async () => {
     setLoading(true);
-    const res = await getTop5SocialDetails({ address: ["0x983d6466FAC5B2aFe57e070283a4932A1bd0508F"] });
+    const res = await getTop5SocialDetails({
+      address: ["0x983d6466FAC5B2aFe57e070283a4932A1bd0508F"],
+    });
 
     // console.log(res);
     // res?.SocialFollowers?.Follower[0].followerAddress.socials[0].userAssociatedAddresses[0]
@@ -147,10 +83,6 @@ const InviteModal = () => {
 
   useEffect(() => {
     fnGetTop5Interactions();
-    create_a_client();
-
-    // fnCreateXMTPClient();
-    // fnNewConversation();
   }, []);
 
   return (
@@ -202,16 +134,12 @@ const InviteModal = () => {
           <div className="flex justify-center m-8"> -- OR -- </div>
 
           <div className="mt-2">
-            <Input
-              label="Wallet Address"
-              name="xmtpSendAddr"
-              onChange={(e) => handleChange(e)}
-            />
+            <Input label="Wallet Address" name="xmtpSendAddr" />
           </div>
 
           <div className="mt-4 text-right">
             <Button
-              onClick={() => fnSendXmtpMessage()}
+              onClick={XMTPfn}
               color="indigo"
               // size="sm"
             >
--- src/hooks/xmtp/useXMTPClient.js
+++ src/hooks/xmtp/useXMTPClient.js
@@ -1,28 +1,6 @@
-import { Client } from "@xmtp/xmtp-js";
-import { useWalletClient } from "wagmi";
-import { XMTP_ENV } from "../../services";
 
 const useXMTPClient = () => {
-  const { data: walletClient } = useWalletClient();
-  const [isError, setIsError] = useState(false);
-  const [error, setError] = useState(null);
-    const [xmtp, setXMTP] = useState(null);
 
-  const client = async () => {
-    try {
-      const xmtp = await Client.create(walletClient, { env: XMTP_ENV })
-      return setXMTP(xmtp);
-    } catch (error) {
-      setIsError(true);
-      setError(error);
-    }
-  }
-
-  return {
-    client,
-    isError,
-    error,
-  };
 };
 
 export default useXMTPClient;
