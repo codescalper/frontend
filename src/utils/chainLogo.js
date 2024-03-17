4580ed6550138fd4edd54bc8c6dbe7d35863168b
Pankaj Kumar <pankajkumardas727@gmail.com>
2024-03-17 20:46:31 +0530

    fix: network

--- src/utils/chainLogo.js
+++ src/utils/chainLogo.js
@@ -7,14 +7,17 @@ import {
 } from "../assets";
 
 const chainLogos = {
-  1: EthLogo,
-  5: EthLogo,
-  8453: BaseLogo,
-  7777777: ZoraLogo,
-  10: OpMainnetLogo,
-  42161: arbitrumLogo,
+  1: EthLogo, // Ethereum
+  5: EthLogo, // Goerli
+  11155111: EthLogo, // Sepolia
+  8453: BaseLogo, // Base
+  84532: BaseLogo, // Base Sepolia
+  7777777: ZoraLogo, // Zora
+  10: OpMainnetLogo, // Optimism
+  42161: arbitrumLogo, // Arbitrum
 };
 
 export const chainLogo = (chainId) => {
+  console.log(chainId);
   return chainLogos[chainId] || null;
 };
